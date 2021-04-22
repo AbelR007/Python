001b47ec97815bd7562689f8f35ac34c0817ae68
Abel Roy <abelroi007@gmail.com>
2021-04-22 20:50:28 +0530

    Create code.py
    A simple python code for calculating simple calculations such as Addition, Subtraction, Division, Multiplication, Square Root, and Exponential. This code was written a long ago, so it may have its faults.

---
+++ Simple Calculator 2.0/code.py
@@ -0,0 +1,37 @@
+#Coding for a Simple Calculator 2.0
+#Performs Addition, Subtraction, Multiplication, Division, Square, and Square root (possibliity)
+a = float(input('Enter the first no. : ')) #Input the two numbers for calculations
+b = float(input('Enter the second no. : '))
+print(" ")
+
+print('Your Menu for Calculations...')
+print('Enter -a- for Addition.')       #Helps give menu
+print('Enter -s- for Subtraction. ')
+print('Enter -m- for Multiplication. ')
+print('Enter -d- for Division. ')
+print('Enter -sq- for square. ')
+print('Enter -sr- for square root. ')
+
+x =  input('Enter the menu : ')
+print()
+
+if x == 'a' :
+  c = a+b
+  print('Your answer for',a,'+',b,'is',c)
+elif x == 's' :
+	print('Your answer for',a,'-',b,'is',a-b)
+elif x == 'm' :
+	print('Your answer for',a,'*',b,'is',a*b)
+elif x == 'd' :
+	print('Your answer for',a,'/',b,'is',a/b)
+elif x == 'sq' :
+	print('Your answer for',a,'^',b,'is',a**b)
+elif x == 'sr' :
+  print('Your answer for',a,'^^',b,'is',a**(1/b))
+
+print()
+print('Thank you for using Simple Calculator 2.0 ...')
+
+#That's How we end it
+#Thank you to you all for checking out my code...And come to my repository regularly...
+#Star it
