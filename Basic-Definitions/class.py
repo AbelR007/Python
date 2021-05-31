3cd7ef0fd602080372d1ba9b8ef00293d04c1e90
Abel Roy <abelroi007@gmail.com>
2021-05-31 22:57:18 +0530

    Updated to a better understanding with subclasses

--- Basic-Definitions/class.py
+++ Basic-Definitions/class.py
@@ -1,8 +1,12 @@
+# If you are reading this from Github,
+# THESE CODES ARE FOR UNDERSTANDING PURPOSES. YOU MAY USE THEM IN ANY WAY YOU WANT.
+# These are not necessarily by me. They are developed by referring through many sources
+
 import datetime
 
 class Employee:
     """docstring for Employee."""
-    raise_amount = 1.04
+    raise_amt = 1.04
     num = 0
 
     def __init__(self, first, last, pay):
@@ -16,11 +20,11 @@ class Employee:
         return "{} {}".format(self.first, self.last)
 
     def apply_raise(self):
-        self.pay = int(self.pay * self.raise_amount)
+        self.pay = int(self.pay * self.raise_amt)
 
     @classmethod
     def set_raise_amt(cls, amount):
-        cls.raise_amount = amount
+        cls.raise_amt = amount
 
     @classmethod
     def from_string(cls, emp_str):
@@ -29,40 +33,45 @@ class Employee:
 
     @staticmethod
     def is_workday(day):
-        if day.weekday() == 5 or day.weekday() == 6 :
+        if day.weekday() == 5 or day.weekday() == 6:
             return False
         return True
 
+
 class Developer(Employee):
-    pass
+    raise_amt = 1.10
 
-emp1 = Developer('Abel', 'Roy', 50000)
-emp2 = Developer('Test', 'User', 60000)
+    def __init__(self, first, last, pay, prog_lang):
+        super().__init__(first, last, pay)
+        self.prog_lang = prog_lang
 
-print(emp1.email)
-print(emp2.email)
 
-print("Done.")
+class Manager(Employee):
+
+    def __init__(self, first, last, pay, employees=None):
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+    def print_emps(self):
+        for emp in self.employees:
+            print("-->", emp.fullname())
+
+
+dev1 = Developer('Abel', 'Roy', 50000, "Python")
+dev2 = Developer('Test', 'User', 60000, "JS")
+mgr1 = Manager("Abel2", "Roy2", 90000, [dev1])
+
+# print("Done.")
 # =========================================================
-# ===== Not needed =========
-"""emp1.set_raise_amt(1.05)
-
-print(Employee.raise_amount)
-print(emp1.raise_amount)
-print(emp2.raise_amount)
-"""
-
-"""
-newemp1 = 'John-Doe-7000'
-first, last, pay = newemp1.split("-")
-newempdata = Employee(first, last, pay)
-newempdata = Employee.from_string(newemp1)
-
-print(newempdata.email)
-print(newempdata.pay)
-"""
-"""
-mydate = datetime.date(2016,7,11)
-print(Employee.is_workday(mydate))
-
-"""
+# CREATED with :heart: by AbelR007
