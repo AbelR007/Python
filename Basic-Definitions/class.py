a25dd80e76e68e7d8b4d710906042f4eed5ba2a6
Abel Roy <abelroi007@gmail.com>
2021-06-02 15:03:44 +0530

    Final Commit

--- Basic-Definitions/class.py
+++ Basic-Definitions/class.py
@@ -1,9 +1,13 @@
+# TOPIC : Python Classes & their basic uses
+
 # If you are reading this from Github,
 # THESE CODES ARE FOR UNDERSTANDING PURPOSES. YOU MAY USE THEM IN ANY WAY YOU WANT.
 # These are not necessarily by me. They are developed by referring through many sources
 
+# Already installed through Python3 | Not necessary module for classes
 import datetime
 
+# Python Main CLASS
 class Employee:
     """docstring for Employee."""
     raise_amt = 1.04
@@ -13,11 +17,27 @@ class Employee:
         self.first = first
         self.last = last
         self.pay = pay
-        self.email = first + '.' + last + '@gmail.com'
         Employee.num += 1
-
+    
+    @property
+    def email(self):
+        return "{}.{}@gmail.com".format(self.first, self.last)
+    
+    @property
     def fullname(self):
         return "{} {}".format(self.first, self.last)
+    
+    @fullname.setter
+    def fullname(self, name):
+        first, last = name.split(' ')
+        self.first = first
+        self.last = last
+    
+    @fullname.deleter
+    def fullname(self):
+        print("Deleted by AbelR007!")
+        self.first = None
+        self.last = None
 
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amt)
@@ -36,8 +56,17 @@ class Employee:
         if day.weekday() == 5 or day.weekday() == 6:
             return False
         return True
+    
+    def __repr__(self):
+        return "Employee('{}','{}',{})".format(self.first, self.last, self.pay)
+    
+    def __str__(self):
+        return "{} - {}".format(self.fullname(),self.email)
+    
+    def __len__(self):
+        return len(self.fullname())
 
-
+# SUB CLASS of EMPLOYEE
 class Developer(Employee):
     raise_amt = 1.10
 
@@ -45,7 +74,7 @@ class Developer(Employee):
         super().__init__(first, last, pay)
         self.prog_lang = prog_lang
 
-
+# SUB CLASS of EMPLOYEE
 class Manager(Employee):
 
     def __init__(self, first, last, pay, employees=None):
@@ -67,11 +96,15 @@ class Manager(Employee):
         for emp in self.employees:
             print("-->", emp.fullname())
 
+emp1 = Employee('Abel', 'Roy', 50000)
+emp2 = Employee('Test', 'User', 60000)
 
 dev1 = Developer('Abel', 'Roy', 50000, "Python")
 dev2 = Developer('Test', 'User', 60000, "JS")
-mgr1 = Manager("Abel2", "Roy2", 90000, [dev1])
 
-# print("Done.")
+mgr1 = Manager("Super Abel", "Super Roy", 90000, [dev1])
+mgr2 = Manager("Super Test","Super User", 100000, [dev1, dev2])
+
 # =========================================================
-# CREATED with :heart: by AbelR007
+# CREATED with ❤️ by AbelR007
+# =========================================================
