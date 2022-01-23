296b5bd3e5df99337bf6abee65a6bbe5b06b3d56
Abel Roy <abelroi007@gmail.com>
2022-01-23 23:03:31 +0530

    New File : Factorial

---
+++ Learn The Basics/factorial.py
@@ -0,0 +1,29 @@
+# FACTORIAL of a NUMBER using Loops
+# ----------------------------------------------------------------
+"""
+    # CONCEPT :
+
+    > Product of an integer and all the integers below it
+    > Example :
+        2! = 1 x 2
+        3! = 1 x 2 x 3
+        4! = 1 x 2 x 3 x 4
+
+"""
+# ----------------------------------------------------------------
+# CODE :
+# ----------------------------------------------------------------
+print("Factorial of a Number\n-------------------")
+fact = 1
+num = int(input("Enter number :"))
+
+if num <= 0 :
+    # Negative numbers don't have a factorial value
+    print("Please enter a positive number! Factorial doesn't exist for negative numbers.")
+else :
+    # Main Logic of the Factorial function
+    for i in range(1,num+1):
+        fact = fact * i
+    print("Factorial of a Number is ",fact)
+
+#---- CODE by Abel Roy | AbelR007 ----#
