223cde81bed2032c1e405a364993a28e8c3d6712
Abel Roy <abelroi007@gmail.com>
2022-03-12 00:06:58 +0530

    Addition 1.7 LTB [Odd/Even and Print commands]

---
+++ Learn The Basics/odd_or_even_number.py
@@ -0,0 +1,15 @@
+# Odd or Even number
+# ----------------------------------------------------------------
+while True:
+    num = int(input("Enter a number: "))
+    #  Breaks out of the loop if you enter "0"
+    if (num == 0)
+        break
+
+    # Checks if a number is even or not
+    if (num % 2 == 0):
+        print(f"{num} is an even number")
+    else:
+        print(f"{num} is an odd number")
+# ----------------------------------------------------------------
+# Code by Abel Roy
