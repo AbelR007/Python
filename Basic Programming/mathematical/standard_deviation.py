2e261b2a52bb415491fe1c3ef96b73037f44081b
AbelR007 <abelroi007@gmail.com>
2022-07-30 23:37:27 +0530

    v1.9.5 - Mathematics

--- Basic Programming/mathematical/standard_deviation.py
+++ Basic Programming/mathematical/standard_deviation.py
@@ -1,15 +1,21 @@
+# Calculating Standard Deviation from matrix
+# =================================================
 import statistics as stats
+
 x = [1,2,3,4,67]
 print(stats.stdev(x))
 
-def matrix(x):
+def standard_deviation(x):
+    # Prints the standard deviation of each row in the matrix
     for i in range(len(x)):
         print("Standard Deviation : ",stats.stdev(x[i]))
 
-new = [
+matrix = [
     [1,2,3,4,5],
     [6,7,111,9,10],
     [11,12,13,14,15],
     [16,17,18,19,20],
 ]
-matrix(new)
+standard_deviation(matrix)
+# ================================================
+# Code by Abel Roy #
---
+++ Basic Programming/real_life_usages/rule_of_72.py
@@ -0,0 +1,11 @@
+# Rule of 72
+# =================================================
+ror = int(input("Enter rate of return : "))
+years = int(72/ror)
+print(years,"years is enought to double the investment")
+# Vica Versa
+rev_years = int(input("Enter year : "))
+rev_ror = int(72/rev_years)
+print(rev_ror,"\b% is the expected rate of return")
+# ================================================
+# Code by Abel Roy #
