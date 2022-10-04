0d0119023f0f418fcb15c801e1148fb6676a2ec7
AbelR007 <abelroi007@gmail.com>
2022-10-04 21:57:20 +0530

    v1.9.19 - GP updated

--- Problems Practice/Arithmetic Progression/gp_with_common_ratio.py
+++ Problems Practice/Arithmetic Progression/gp_with_common_ratio.py
@@ -15,14 +15,17 @@
 
 """
 # =================================================
+# Given Constants
+arr = [ -3, 5, 0, 2, 1, 25, 25, 100 ]
+k = 5
+out = 6
+# =================================================
+# CODE
 def sum(arr):
     sum = 0
     for i in range(len(arr)):
         sum += arr[i]
     return sum
-
-# def gp_or_not(arr):
-
 def Solution(arr, k):
 
     length = len(arr)
@@ -31,7 +34,6 @@ def Solution(arr, k):
 
         first_half = arr[:pos]
         second_half = arr[pos+1:]
-        print(first_half, second_half)
 
         sum_first = sum(first_half)
         sum_second = sum(second_half)
@@ -41,16 +43,56 @@ def Solution(arr, k):
             if (i/sum_first) == (sum_second/i):
                 return pos+1
         except ZeroDivisionError:
-            print("Zero hua")
+            pass
     return 0
 
-arr = [ -3, 5, 0, 2, 1, 25, 25, 100 ]
-k = 5
-out = 6
-
 x = Solution(arr, k)
-print("============")
-print(x)
 print(x==out)
 # ================================================
-# Code by Abel Roy #
+def Solution2(arr, k):
+    n = len(arr)
+    for i in range(1,len(arr)-1):
+        indices = range(i)
+        prefix_sum = sum([arr[p] for p in indices])
+
+        suffix_indices = range(i+1,n)
+        suffix_sum = sum([arr[q] for q in suffix_indices])
+
+        try:
+            val = arr[i]
+            if (val/prefix_sum) == (suffix_sum/val):
+                return i+1
+        except ZeroDivisionError:
+            pass
+    return 0
+
+y = Solution2(arr,k)
+print(y==out)
+# ================================================
+# Above Code by Abel Roy #
+# ================================================
+# Problem Solution [From the internet, not by me]
+def Actual_Solution(arr, k):
+    N = len(arr)
+    if (N < 3):
+        return -1
+    Sum = 0
+    temp = 0
+    for i in range(0, N):
+    	Sum += arr[i]
+    R = (k * k + k + 1)
+    if (Sum % R != 0):
+    	return 0
+    Mid = k * (Sum // R)
+    for i in range(1, N-1):
+    	temp += arr[i - 1]
+    	if (arr[i] == Mid):
+    		if (temp == Mid // k):
+    			return i + 1
+    		else:
+    			return 0
+    return 0
+
+z = Actual_Solution(arr, k)
+print(z==out)
+# ================================================
